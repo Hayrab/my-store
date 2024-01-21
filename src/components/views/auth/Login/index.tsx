a5701d463ffb63bc7b35268c274f23df8a953874
narunhamiya <narunhamiya@gmail.com>
2024-01-22 00:30:39 +0700

    feat: login

---
+++ src/components/views/auth/Login/index.tsx
@@ -0,0 +1,78 @@
+import Link from "next/link";
+import styles from "./Login.module.scss";
+import { useRouter } from "next/router";
+import { FormEvent, useState } from "react";
+import { signIn } from "next-auth/react";
+
+const LoginView = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const { push, query } = useRouter();
+
+  const callbackUrl: any = query.callbackUrl || "/";
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setIsLoading(true);
+    setError("");
+    const form = event.target as HTMLFormElement;
+    try {
+      const res = await signIn("credentials", {
+        redirect: false,
+        email: form.email.value,
+        password: form.password.value,
+        callbackUrl,
+      });
+      if (!res?.error) {
+        setIsLoading(false);
+        form.reset();
+        push(callbackUrl);
+      } else {
+        setIsLoading(false);
+        setError("Email or password is incorrect");
+      }
+    } catch {
+      setIsLoading(false);
+      setError("Email or password is incorrect");
+    }
+  };
+
+  return (
+    <div className={styles.login}>
+      <h1 className={styles.login__title}>Login</h1>
+      <div className={styles.login__form}>
+        <form onSubmit={handleSubmit}>
+          <div className={styles.login__form__item}>
+            <label htmlFor="email">E-Mail</label>
+            <input
+              name="email"
+              id="email"
+              type="text"
+              className={styles.login__form__item__input}
+            />
+          </div>
+          <div className={styles.login__form__item}>
+            <label htmlFor="password">Password</label>
+            <input
+              name="password"
+              id="password"
+              type="text"
+              className={styles.login__form__item__input}
+            />
+          </div>
+          <button type="submit" className={styles.login__form__button}>
+            {isLoading ? "Loadings..." : "Login"}
+          </button>
+        </form>
+      </div>
+      {error && <p className={styles.login__error}>{error}</p>}
+      <p className={styles.login__link}>
+        Don{"'"}t Have an account? Sign Up{" "}
+        <Link href="/auth/register"> Click Here </Link>
+      </p>
+    </div>
+  );
+};
+
+export default LoginView;
